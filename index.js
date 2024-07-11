fbb9812f67fcdd93e733a5df15ad9224b7a76b17
HX <heikosohn@gmail.com>
2024-07-11 13:27:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 // where your node app starts
+//////// SETUP ////////
 
 // init project
 var express = require('express');
@@ -18,12 +19,43 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//////// HELPER FUNCTIONS ////////
+// validate Date
+const isValidDate = (date) => date instanceof Date && !isNaN(date.getTime());
 
+const parseDate = (dateParam) => {
+  // If dateParam is a number, treat it as a Unix timestamp
+  if (!isNaN(dateParam)) {
+    return new Date(parseInt(dateParam));
+  }
+  // Otherwise, treat it as a date string
+  return new Date(dateParam);
+};
+
+//////// ENDPOINTS ////////
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
+
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+
+  // Parse and validate the date
+  let date;
+  if (!dateParam) {
+    date = new Date(); // If no dateParam, use current date
+  } else {
+    date = parseDate(dateParam);
+    if (!isValidDate(date)) {
+      return res.json({ error: "Invalid Date" });
+    }
+  }
+
+  // response with Unix timestamp and UTC string (always GMT time)
+  res.json({
+    unix: date.getTime(), // ex. 1720636325376
+    utc: date.toUTCString()
+  });
+});
 
 
 // Listen on port set in environment variable or default to 3000
